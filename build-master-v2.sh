cb84c8e8efce7ae70ad6fe3f04c502d4373db34c
Shane <shane@smiskol.com>
2020-06-07 22:10:33 -0500

    add build master-v2

---
+++ build-master-v2.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+set -e
+
+mkdir -p /dev/shm
+chmod 777 /dev/shm
+
+
+SOURCE_DIR=/data/openpilot_source
+TARGET_DIR=/data/openpilot_build
+
+ln -sf $TARGET_DIR /data/pythonpath
+
+export GIT_COMMITTER_NAME="ShaneSmiskol"
+export GIT_COMMITTER_EMAIL="shane@smiskol.com"
+export GIT_AUTHOR_NAME="ShaneSmiskol"
+export GIT_AUTHOR_EMAIL="shane@smiskol.com"
+export GIT_SSH_COMMAND="ssh -i /data/gitkey"
+
+VERSION=$(cat selfdrive/common/version.h | awk -F\" '{print $2}')
+echo "#define COMMA_VERSION \"$VERSION-release\"" > selfdrive/common/version.h
+
+cd /data/openpilot
+git checkout master
+git fetch
+git reset --hard origin/master
+git pull
+git clean -xfd
+git submodule update --init
+git submodule foreach --recursive git reset --hard
+git submodule foreach --recursive git clean -xdf
+echo "Done setting up master branch"
+
+rm -f panda/board/obj/panda.bin.signed
+
+SCONS_CACHE=1 scons -j8
+
+echo "[-] committing version $VERSION T=$SECONDS"
+git add -f .
+git status
+git commit -a -m "openpilot v$VERSION release"
+
+git push -f origin master-ci
+echo "Successfully pushed"
